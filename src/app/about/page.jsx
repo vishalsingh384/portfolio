e004d1a376d3c733ee5fc9f3b4ea755d678bd413
Singh <vishalsing384@gmail.com>
2024-09-18 13:24:34 +0530

    :sparkles:added new animations and started portfolio page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,61 +1,65 @@
 'use client'
-import { motion } from 'framer-motion'
+import Brain from '@/components/brain'
+import { animate, easeIn, easeInOut, motion, useInView, useScroll } from 'framer-motion'
 import Image from 'next/image'
+import { useRef } from 'react'
 
 const about = () => {
+
+  const containerRef = useRef();
+  const ref=useRef();
+  const isInView=useInView(ref,{margin:"-100px", once:"true"});  
+
+  const { scrollYProgress } = useScroll({ container: containerRef });
+
   return (
     <motion.div className="h-full" initial={{ y: "-200vh" }} animate={{ y: "0vh" }} transition={{ duration: 1 }}>
       {/* Container */}
-      <div className='h-full'>
+      <div className='h-full bg-red-500 lg:flex overflow-scroll no-scrollbar' ref={containerRef}>
 
 
-        {/* Text Container */}
-        <div className='p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64'>
-          {/* Biography Container */}
-          <div className='flex flex-col gap-12 justify-center'>
-            {/* Biography Image */}
-            <Image
-              src=""
-              alt=""
-              width={112}
-              height={112}
-              className="w-28 h-28 rounded-full object-cover"
-            />
-            {/* Bio Title */}
-            <h1 className='font-bold text-2xl'>BIOGRAPHY</h1>
-            {/* Bio Desc */}
-            <p className='text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem voluptatum consequuntur optio maxime ut, pariatur totam architecto aspernatur possimus debitis, quae, beatae quod? Minima ipsum dolor dolore impedit, repellat id.
-            </p>
-            {/* Bio scroll svg */}
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              width={50}
-              height={50}
-            >
-              <path
-                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
-                stroke="#000000"
-                strokeWidth="1"
-              ></path>
-              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
-              <path
-                d="M15 11L12 14L9 11"
-                stroke="#000000"
-                strokeWidth="1"
-              ></path>
-            </svg>
-          </div>
+      {/* Text Container */}
+      <div className='h-fit border border-blue-400 p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2'>
+
+        {/* Biography Container */}
+        <div className='flex flex-col gap-12 justify-center'>
+          {/* Bio Title */}
+          <h1 className='font-bold text-2xl'>BIOGRAPHY</h1>
+          {/* Bio Desc */}
+          <p className='text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem voluptatum consequuntur optio maxime ut, pariatur totam architecto aspernatur possimus debitis, quae, beatae quod? Minima ipsum dolor dolore impedit, repellat id.
+          </p>
+          {/* Bio scroll svg */}
+          <motion.svg
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            width={50}
+            height={50}
+            initial={{y:"-20px", opacity:0.2}}
+            animate={{y:0, opacity:1}}
+            transition={{repeat:Infinity, duration:2, ease:easeInOut}}
+          >
+            <path
+              d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+              stroke="#000000"
+              strokeWidth="1"
+            ></path>
+            <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+            <path
+              d="M15 11L12 14L9 11"
+              stroke="#000000"
+              strokeWidth="1"
+            ></path>
+          </motion.svg>
         </div>
 
 
         {/* Skills Container */}
-        <div className='flex flex-col gap-12 justify-center'>
+        <motion.div ref={ref} initial={{x:"-300px"}} animate={isInView?{x:0}:{}} className='flex flex-col gap-12 justify-center'>
           {/* Skill Title */}
-          <h1 className='font-bold text-2xl'>SKILLS</h1>
+          <h1 ref={ref} className='font-bold text-2xl'>SKILLS</h1>
           {/* Skill List */}
-          <div className=' flex gap-4 flex-wrap'>
+          <div className='flex gap-4 flex-wrap'>
             <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
               JavaScript
             </div>
@@ -132,141 +136,20 @@ const about = () => {
               Figma
             </div>
           </div>
-          {/* Skill scroll svg */}
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            width={50}
-            height={50}
-          >
-            <path
-              d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
-              stroke="#000000"
-              strokeWidth="1"
-            ></path>
-            <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
-            <path
-              d="M15 11L12 14L9 11"
-              stroke="#000000"
-              strokeWidth="1"
-            ></path>
-          </svg>
-        </div>
-
-
-        {/* EXPERIENCE CONTAINER */}
-        <div
-            className="flex flex-col gap-12 justify-center pb-48"
-          >
-            {/* EXPERIENCE TITLE */}
-            <motion.h1 className="font-bold text-2xl">
-              EXPERIENCE
-            </motion.h1>
-            {/* EXPERIENCE LIST */}
-            <motion.div className="">
-              {/* EXPERIENCE LIST ITEM */}
-              <div className="flex justify-between h-48">
-                {/* LEFT */}
-                <div className="w-1/3 ">
-                  {/* JOB TITLE */}
-                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
-                    Senior JavaScript Engineer
-                  </div>
-                  {/* JOB DESC */}
-                  <div className="p-3 text-sm italic">
-                    I led web development, offering expertise in JavaScript
-                    frameworks.{" "}
-                  </div>
-                  {/* JOB DATE */}
-                  <div className="p-3 text-red-400 text-sm font-semibold">
-                    2024 - Present
-                  </div>
-                  {/* JOB COMPANY */}
-                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                    Apple
-                  </div>
-                </div>
-                {/* CENTER */}
-                <div className="w-1/6 flex justify-center">
-                  {/* LINE */}
-                  <div className="w-1 h-full bg-gray-600 rounded relative">
-                    {/* LINE CIRCLE */}
-                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
-                  </div>
-                </div>
-                {/* RIGHT */}
-                <div className="w-1/3 "></div>
-              </div>
-              {/* EXPERIENCE LIST ITEM */}
-              <div className="flex justify-between h-48">
-                {/* LEFT */}
-                <div className="w-1/3 "></div>
-                {/* CENTER */}
-                <div className="w-1/6 flex justify-center">
-                  {/* LINE */}
-                  <div className="w-1 h-full bg-gray-600 rounded relative">
-                    {/* LINE CIRCLE */}
-                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
-                  </div>
-                </div>
-                {/* RIGHT */}
-                <div className="w-1/3 ">
-                  {/* JOB TITLE */}
-                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
-                    Senior React Developer
-                  </div>
-                  {/* JOB DESC */}
-                  <div className="p-3 text-sm italic">
-                    I spearheaded React-based application development,
-                    leveraging advanced skills.{" "}
-                  </div>
-                  {/* JOB DATE */}
-                  <div className="p-3 text-red-400 text-sm font-semibold">
-                    2019 - 2024{" "}
-                  </div>
-                  {/* JOB COMPANY */}
-                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                    Microsoft
-                  </div>
-                </div>
-              </div>
-              {/* EXPERIENCE LIST ITEM */}
-              <div className="flex justify-between h-48">
-                {/* LEFT */}
-                <div className="w-1/3 ">
-                  {/* JOB TITLE */}
-                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
-                    Freelancer{" "}
-                  </div>
-                  {/* JOB DESC */}
-                  <div className="p-3 text-sm italic">
-                    I provided web solutions, applying a range of technologies
-                    to address client requirements.{" "}
-                  </div>
-                  {/* JOB DATE */}
-                  <div className="p-3 text-red-400 text-sm font-semibold">
-                    2010 - 2019{" "}
-                  </div>
-                </div>
-                {/* CENTER */}
-                <div className="w-1/6 flex justify-center">
-                  {/* LINE */}
-                  <div className="w-1 h-full bg-gray-600 rounded relative">
-                    {/* LINE CIRCLE */}
-                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
-                  </div>
-                </div>
-                {/* RIGHT */}
-                <div className="w-1/3 "></div>
-              </div>
-            </motion.div>
-          </div>
-        {/* SVG CONTAINER */}
-        <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2">
-        </div>
+        </motion.div>
+        
       </div>
-    </motion.div>
+
+
+
+      {/* SVG CONTAINER */}
+      <div className="hidden lg:flex lg:w-1/3 sticky top-0 z-30 xl:w-1/2">
+        <Brain scrollYProgress={scrollYProgress} />
+      </div>
+
+
+    </div>
+    </motion.div >
   )
 }
 
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,12 @@
 'use client'
 import {motion} from 'framer-motion'
 const portfolio = () => {
+  const ref=useRef();
     return (
       <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0vh"}} transition={{duration:1}}>
-        Portfolio
+        <div className='h-[600vh]' ref={ref}>
+          <div></div>
+        </div>
       </motion.div>
     )
   }
