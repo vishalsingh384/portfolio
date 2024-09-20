f4c6ee6fd8e5fe82e5e31c48cb0d655302ab42fe
Singh <vishalsing384@gmail.com>
2024-09-20 16:50:23 +0530

    :sparkles: contact page updated

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,28 +1,58 @@
 'use client'
 import { motion } from 'framer-motion'
+import { useState, useRef } from 'react'
+import emailjs from '@emailjs/browser';
+
 
 const contact = () => {
   const text = "Hello World"
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setError(false);
+    setSuccess(false);
+
+    emailjs
+      .sendForm(process.env.NEXT_PUBLIC_SERVICE_ID, process.env.NEXT_PUBLIC_TEMPLATE_ID, form.current, {
+        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          setSuccess(true);
+          form.current.reset();
+        },
+        (error) => {
+          setError(true);
+        },
+      );
+  };
+
   return (
     <motion.div className="h-full" initial={{ y: "-200vh" }} animate={{ y: "0vh" }} transition={{ duration: 1 }}>
       <div className='h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
         {/* Text Container */}
         <div className='h-1/2 lg:h-full lg:w-1/2'>
-          {text.split("").map((letter, index)=>(
-            <motion.span key={index} initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:2, repeat:Infinity,  repeatType: "reverse", delay:index*0.1}}>
+          {text.split("").map((letter, index) => (
+            <motion.span key={index} initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 2, repeat: Infinity, repeatType: "reverse", delay: index * 0.1 }}>
               {letter}
             </motion.span>
           ))}
           😁
         </div>
         {/* Form Container */}
-        <form className='h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24'>
-        <span>Hey V,</span>
-        <textarea className='bg-transparent border-b-2 border-b-black outline-none resize-none'/>
-        <span>My mail address is:</span>
-        <input type='text' className='bg-transparent border-b-2 border-b-black outline-none'/>
-        <span>Regards</span>
-        <button className='bg-purple-200 rounded font-semibold text-gray-600 p-4'>Send 📩</button>
+        <form onSubmit={sendEmail} ref={form} className='h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-15 justify-center p-24'>
+          <span>Hey V,</span>
+          <textarea rows={6} className='bg-transparent border border-red-500 outline-none resize-none' name="user_message" />
+          <span>My mail address is:</span>
+          <input name="user_name" type='text' className='bg-transparent border-b-2 border-b-black outline-none' />
+          <span>Regards</span>
+          <button className='bg-purple-200 rounded font-semibold text-gray-600 p-4'>Send 📩</button>
+          {success && <span className='text-green-600 semibold'>Message sent successfully!</span>}
+          {error && <span className='text-red-600 semibold'>Something went wrong</span>}
         </form>
       </div>
     </motion.div>
